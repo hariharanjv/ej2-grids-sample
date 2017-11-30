787ce2883889a7d56b6319aecc0d56aa83b1e1f6
ROOT\hariharanjv <hariharanjv@syncfusion.com>
2017-11-30 18:44:11 +0530

    Sample updated

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { gridData1 } from './data';
+import { OrderData } from './data';
 import { ToolbarItem } from '@syncfusion/ej2-grids';
 
 @Component({
-  selector: 'app-container',
+  selector: 'my-app',
   template: ` <ej-grid #grid [dataSource]='data'  height='350px' [editSettings]='editSetting' [toolbar]='items'>
                 <e-columns>
                     <e-column field='OrderID' isPrimaryKey='true' headerText='Order ID' textAlign='right' width=120></e-column>
@@ -22,7 +22,7 @@ export class AppComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.data = gridData1;
+    this.data = OrderData;
     this.items = [ToolbarItem.Add, ToolbarItem.Edit, ToolbarItem.Delete, ToolbarItem.Update, ToolbarItem.Cancel];
     this.validationRules = { required: true };
     this.editSetting = { allowEditing: true, allowDeleting: true, allowAdding: true };
